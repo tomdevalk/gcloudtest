e6480aa34f4fde7d070de3501e3c4aeab9ec212c
tomdevalk <36774076+tomdevalk@users.noreply.github.com>
2018-02-26 12:10:52 -0500

    Add files via upload

---
+++ googletest.py
@@ -0,0 +1,39 @@
+def run_quickstart():
+    # [START vision_quickstart]
+    import io
+    import os
+
+    # Imports the Google Cloud client library
+    # [START migration_import]
+    from google.cloud import vision
+    from google.cloud.vision import types
+    # [END migration_import]
+
+    # Instantiates a client
+    # [START migration_client]
+    client = vision.ImageAnnotatorClient()
+    # [END migration_client]
+
+    # The name of the image file to annotate
+    file_name = os.path.join(
+        os.path.dirname(__file__),
+        'ppjhns.jpg')
+
+    # Loads the image into memory
+    with io.open(file_name, 'rb') as image_file:
+        content = image_file.read()
+
+    image = types.Image(content=content)
+
+    # Performs label detection on the image file
+    response = client.logo_detection(image=image)
+    logos = response.logo_annotations
+
+    print('Logos:')
+    for logo in logos:
+        print(logo.description, logo.confidence)
+    # [END vision_quickstart]
+
+
+if __name__ == '__main__':
+    run_quickstart()
