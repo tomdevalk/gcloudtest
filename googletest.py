e4691cdca82f7ccdf8005657a4f1602eb11aa906
tomdevalk <36774076+tomdevalk@users.noreply.github.com>
2018-02-26 13:10:36 -0500

    Add files via upload

--- googletest.py
+++ googletest.py
@@ -17,7 +17,7 @@ def run_quickstart():
     # The name of the image file to annotate
     file_name = os.path.join(
         os.path.dirname(__file__),
-        'o.jpg')
+        'bk-chicken-fries-hed-2014.jpg')
 
     # Loads the image into memory
     with io.open(file_name, 'rb') as image_file:
